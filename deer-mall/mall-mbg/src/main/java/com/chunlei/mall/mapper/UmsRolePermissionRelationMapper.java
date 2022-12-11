6bd4db7a5f1c264bcdfb2fda1323b24a4c25c9c0
luchunlei42 <112138702+luchunlei42@users.noreply.github.com>
2022-12-10 16:54:02 -0800

    first commit

---
+++ deer-mall/mall-mbg/src/main/java/com/chunlei/mall/mapper/UmsRolePermissionRelationMapper.java
@@ -0,0 +1,30 @@
+package com.chunlei.mall.mapper;
+
+import com.chunlei.mall.model.UmsRolePermissionRelation;
+import com.chunlei.mall.model.UmsRolePermissionRelationExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UmsRolePermissionRelationMapper {
+    long countByExample(UmsRolePermissionRelationExample example);
+
+    int deleteByExample(UmsRolePermissionRelationExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsRolePermissionRelation record);
+
+    int insertSelective(UmsRolePermissionRelation record);
+
+    List<UmsRolePermissionRelation> selectByExample(UmsRolePermissionRelationExample example);
+
+    UmsRolePermissionRelation selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsRolePermissionRelation record, @Param("example") UmsRolePermissionRelationExample example);
+
+    int updateByExample(@Param("record") UmsRolePermissionRelation record, @Param("example") UmsRolePermissionRelationExample example);
+
+    int updateByPrimaryKeySelective(UmsRolePermissionRelation record);
+
+    int updateByPrimaryKey(UmsRolePermissionRelation record);
+}
